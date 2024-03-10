50f1c29329a562d6c0fab1e4fb6beece5dce0302
Mateus Cristofori <ajhororow12@gmail.com>
2024-03-09 21:15:04 -0300

    Adding jquery in to the project

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,12 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<div>
+    <h1>SALVE</h1>
+    <button id="click_button">Click</button>
+</div>
+
+<script>
+    $("#click_button").on('click', function() {
+        console.log('SALVE');
+    });
+</script>
